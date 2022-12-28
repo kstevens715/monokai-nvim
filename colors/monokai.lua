a8aecf44fadcd7430aee0827827744b881a5aa89
Kyle Stevens <kstevens715@gmail.com>
2022-12-28 08:59:37 -0500

    Rename colors

--- colors/monokai.lua
+++ colors/monokai.lua
@@ -11,72 +11,71 @@ vim.cmd([[
 ]])
 
 local black                = '#000000'
+local blue                 = '#204a87'
 local bright_green         = '#a6e22e'
 local bright_pink          = '#f92672'
 local comment_grey         = '#75715e'
-local light_grey           = '#3c3d37'
-local light_text           = '#f8f8f0'
-local lighter_grey         = '#90908a'
-local purple               = '#ae81ff'
-local sky_blue             = '#66d9ef'
-local status_back          = '#64645e'
-local string_yellow        = '#e6db74'
 local diff_add             = '#46830c'
-
-local unnamed7 = '#204a87'
-local unnamed9 = '#272822'
-local unnamed14 = '#49483e'
-local unnamed17 = '#fd971f'
-local unnamed18 = '#8b0807'
-local unnamed20 = '#c4be89'
+local grey1                = '#90908a'
+local grey2                = '#64645e'
+local grey3                = '#49483e'
+local grey4                = '#3c3d37'
+local grey5                = '#272822'
+local light_text           = '#f8f8f0'
+local orange               = '#fd971f'
+local purple               = '#ae81ff'
+local red                  = '#8b0807'
+local sky_blue             = '#66d9ef'
+local string_yellow        = '#e6db74'
+local string_yellow_darker = '#c4be89'
 
 -- Base Styles
 vim.api.nvim_set_hl(0, 'Character', { fg = purple })
-vim.api.nvim_set_hl(0, 'ColorColumn', { bg = light_grey })
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = grey4 })
 vim.api.nvim_set_hl(0, 'Conceal', { fg = light_text })
 vim.api.nvim_set_hl(0, 'Conditional', { fg = bright_pink })
-vim.api.nvim_set_hl(0, 'Cursor', { fg = unnamed9, bg = light_text })
-vim.api.nvim_set_hl(0, 'CursorColumn', { bg = light_grey })
-vim.api.nvim_set_hl(0, 'CursorLine', { bg = light_grey })
+vim.api.nvim_set_hl(0, 'Cursor', { fg = grey5, bg = light_text })
+vim.api.nvim_set_hl(0, 'CursorColumn', { bg = grey4 })
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = grey4 })
 vim.api.nvim_set_hl(0, 'Define', { fg = bright_pink })
 vim.api.nvim_set_hl(0, 'DiffAdd', { fg = light_text, bg = diff_add, bold = true })
-vim.api.nvim_set_hl(0, 'DiffChange', { fg = light_text, bg = light_grey })
-vim.api.nvim_set_hl(0, 'DiffDelete', { fg = unnamed18 })
-vim.api.nvim_set_hl(0, 'DiffText', { fg = light_text, bg = unnamed7, bold = true })
+vim.api.nvim_set_hl(0, 'DiffChange', { fg = light_text, bg = grey4 })
+vim.api.nvim_set_hl(0, 'DiffDelete', { fg = red })
+vim.api.nvim_set_hl(0, 'DiffText', { fg = light_text, bg = blue, bold = true })
 vim.api.nvim_set_hl(0, 'Directory', { fg = purple })
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { fg = unnamed9, bg = unnamed9 })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { fg = grey5, bg = grey5 })
 vim.api.nvim_set_hl(0, 'ErrorMsg', { bg = bright_pink, fg = light_text })
-vim.api.nvim_set_hl(0, 'Folded', { fg = comment_grey, bg = unnamed9 })
-vim.api.nvim_set_hl(0, 'IncSearch', { fg = unnamed20, bg = black, reverse = true })
+vim.api.nvim_set_hl(0, 'Folded', { fg = comment_grey, bg = grey5 })
+vim.api.nvim_set_hl(0, 'IncSearch', { fg = string_yellow_darker, bg = black, reverse = true })
 vim.api.nvim_set_hl(0, 'Label', { fg = string_yellow })
-vim.api.nvim_set_hl(0, 'LineNr', { bg = light_grey, fg = lighter_grey })
+vim.api.nvim_set_hl(0, 'LineNr', { bg = grey4, fg = grey1 })
 vim.api.nvim_set_hl(0, 'MatchParen', { fg = bright_pink, underline = true })
 vim.api.nvim_set_hl(0, 'NonText', { fg = comment_grey })
-vim.api.nvim_set_hl(0, 'Normal', { fg = light_text, bg = unnamed9 })
-vim.api.nvim_set_hl(0, 'NormalFloat', { fg = string_yellow, bg = light_grey })
-vim.api.nvim_set_hl(0, 'PmenuSel', { bg = unnamed14 })
+vim.api.nvim_set_hl(0, 'Normal', { fg = light_text, bg = grey5 })
+vim.api.nvim_set_hl(0, 'NormalFloat', { fg = string_yellow, bg = grey4 })
+vim.api.nvim_set_hl(0, 'PmenuSel', { bg = grey3 })
 vim.api.nvim_set_hl(0, 'PreProc', { fg = bright_pink })
-vim.api.nvim_set_hl(0, 'Search', { fg = light_text, bg = unnamed7 })
-vim.api.nvim_set_hl(0, 'SignColumn', { bg = light_grey })
+vim.api.nvim_set_hl(0, 'Search', { fg = light_text, bg = blue })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = grey4 })
 vim.api.nvim_set_hl(0, 'Special', { fg = light_text })
 vim.api.nvim_set_hl(0, 'SpecialChar', { fg = purple })
 vim.api.nvim_set_hl(0, 'Statement', { fg = bright_pink })
-vim.api.nvim_set_hl(0, 'StatusLine', { fg = light_text, bg = status_back, bold = true })
-vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = light_text, bg = status_back })
+vim.api.nvim_set_hl(0, 'StatusLine', { fg = light_text, bg = grey2, bold = true })
+vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = light_text, bg = grey2 })
 vim.api.nvim_set_hl(0, 'StorageClass', { fg = sky_blue, italic = true })
 vim.api.nvim_set_hl(0, 'Tag', { fg = bright_pink })
 vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = bright_green })
 vim.api.nvim_set_hl(0, 'Title', { fg = light_text, bold = true })
 vim.api.nvim_set_hl(0, 'Type', { fg = bright_pink })
 vim.api.nvim_set_hl(0, 'Underlined', { underline = true })
-vim.api.nvim_set_hl(0, 'VertSplit', { fg = status_back, bg = status_back })
-vim.api.nvim_set_hl(0, 'Visual', { bg = unnamed14 })
+vim.api.nvim_set_hl(0, 'VertSplit', { fg = grey2, bg = grey2 })
+vim.api.nvim_set_hl(0, 'Visual', { bg = grey3 })
 vim.api.nvim_set_hl(0, 'WarningMsg', { bg = bright_pink, fg = light_text })
-vim.api.nvim_set_hl(0, 'diffAdded', { fg = bright_green, bg = light_grey })
+vim.api.nvim_set_hl(0, 'diffAdded', { fg = bright_green, bg = grey4 })
 vim.api.nvim_set_hl(0, 'diffFile', { fg = sky_blue })
 vim.api.nvim_set_hl(0, 'diffIndexLine', { fg = comment_grey })
 vim.api.nvim_set_hl(0, 'diffLine', { fg = sky_blue })
-vim.api.nvim_set_hl(0, 'diffRemoved', { fg = bright_pink, bg = light_grey })
+vim.api.nvim_set_hl(0, 'diffRemoved', { fg = bright_pink, bg = grey4 })
 vim.api.nvim_set_hl(0, 'diffSubname', { fg = comment_grey })
 vim.api.nvim_set_hl(0, 'gitcommitComment', { fg = comment_grey })
 vim.api.nvim_set_hl(0, 'helpCommand', { fg = bright_green })
@@ -91,12 +90,12 @@ vim.api.nvim_set_hl(0, 'WhichKey', { fg = bright_green })
 vim.api.nvim_set_hl(0, 'WhichKeyGroup', { fg = sky_blue })
 vim.api.nvim_set_hl(0, 'WhichKeySeparator', { fg = bright_pink })
 vim.api.nvim_set_hl(0, 'WhichKeyDesc', { fg = sky_blue })
-vim.api.nvim_set_hl(0, 'WhichKeyFloat', { fg = string_yellow, bg = light_grey })
-vim.api.nvim_set_hl(0, 'WhichKeyBorder', { fg = lighter_grey, bg = light_grey })
+vim.api.nvim_set_hl(0, 'WhichKeyFloat', { fg = string_yellow, bg = grey4 })
+vim.api.nvim_set_hl(0, 'WhichKeyBorder', { fg = grey1, bg = grey4 })
 vim.api.nvim_set_hl(0, 'WhichKeyValue', { fg = sky_blue })
 
 -- Treesitter Styles
-vim.api.nvim_set_hl(0, '@block_parameters', { fg = unnamed17, italic = true })
+vim.api.nvim_set_hl(0, '@block_parameters', { fg = orange, italic = true })
 vim.api.nvim_set_hl(0, '@boolean', { fg = purple })
 vim.api.nvim_set_hl(0, '@comment', { fg = comment_grey })
 vim.api.nvim_set_hl(0, '@function', { fg = bright_green })
@@ -107,6 +106,6 @@ vim.api.nvim_set_hl(0, '@string', { fg = string_yellow })
 vim.api.nvim_set_hl(0, '@symbol', { fg = sky_blue })
 vim.api.nvim_set_hl(0, '@tag', { fg = bright_pink })
 vim.api.nvim_set_hl(0, '@tag.attribute', { fg = bright_green })
-vim.api.nvim_set_hl(0, '@tag.delimiter', { fg = lighter_grey })
+vim.api.nvim_set_hl(0, '@tag.delimiter', { fg = grey1 })
 vim.api.nvim_set_hl(0, '@type', { fg = sky_blue })
 vim.api.nvim_set_hl(0, '@variable', { fg = light_text })
